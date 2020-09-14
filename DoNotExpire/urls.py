a1e37325723af0b4bcf260dfd9f9cb8e653053ed
Alschn <adam.lisichin@gmail.com>
2020-09-14 19:18:51 +0200

    Add login/register forms
    Added forms and their corresponding routes, new templates and simple styling.

--- DoNotExpire/urls.py
+++ DoNotExpire/urls.py
@@ -19,10 +19,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from DoNotExpire.manager import views as manager_views
+from DoNotExpire.profiles import views as profiles_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('DoNotExpire.manager.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='profiles/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='profiles/logout.html'), name='logout'),
+    path('register/', profiles_views.register, name='register'),
 ]
 
 if settings.DEBUG:
