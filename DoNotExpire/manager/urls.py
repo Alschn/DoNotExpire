9ccec2a78978325afc688a9991cc231130e63d52
Alschn <adam.lisichin@gmail.com>
2020-09-19 19:48:10 +0200

    Create/Delete works for accounts and characters
    Adding new character and account is now possible from homepage, one can easily delete the account but delete char form can be accesed only with the url for now

--- DoNotExpire/manager/urls.py
+++ DoNotExpire/manager/urls.py
@@ -3,8 +3,9 @@ from . import views as manager_views
 
 urlpatterns = [
     path('', manager_views.home, name='home'),
-    path('newacc/', manager_views.create_account, name='create-account'),
-    path('newchar/', manager_views.create_char, name='create-char'),
+    path('new/acc/', manager_views.create_account, name='create-account'),
+    path('<str:pk>/new/char/', manager_views.create_char, name='create-char'),
     path('update_date/<str:name>/', manager_views.update_date, name='update-date'),
-    path('delete/', manager_views.delete_char, name='delete-char'),
+    path('delete/char/<int:pk>', manager_views.CharacterDeleteView.as_view(), name='delete-char'),
+    path('delete/acc/<int:pk>', manager_views.AccountDeleteView.as_view(), name='delete-acc'),
 ]
