1153b2ddc96198dca4c9b08e35bd4e04172307a7
Alschn <adam.lisichin@gmail.com>
2020-09-19 14:20:04 +0200

    Date update button working
    Now you can click the Update button to alter character's last_visited field

--- DoNotExpire/manager/urls.py
+++ DoNotExpire/manager/urls.py
@@ -3,6 +3,8 @@ from . import views as manager_views
 
 urlpatterns = [
     path('', manager_views.home, name='home'),
-    path('newacc/', manager_views.create_account, name='create_account'),
-    path('newchar/', manager_views.create_char, name='create_char'),
+    path('newacc/', manager_views.create_account, name='create-account'),
+    path('newchar/', manager_views.create_char, name='create-char'),
+    path('update_date/<str:name>/', manager_views.update_date, name='update-date'),
+    path('delete/', manager_views.delete_char, name='delete-char'),
 ]
--- DoNotExpire/manager/views.py
+++ DoNotExpire/manager/views.py
@@ -1,16 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.utils import timezone
+import datetime
 from .forms import CreateAccountForm, CreateCharacterForm
+from .models import Character
 
 
 def home(request):
-    return render(request, 'manager/index.html')
+    user_accounts = request.user.profile.accounts.all()
+
+    context = {
+        'user_accounts': user_accounts,
+    }
+    return render(request, 'manager/index.html', context)
 
 
 def create_char(request):
-    # c_form = CreateCharacterForm()
+    # if current account has 16 chars, then redirect to homepage with message
+    # else create form for char creation
+    if request.method == "POST":
+        c_form = CreateCharacterForm(request.POST)
+        if c_form.is_valid():
+            return redirect('home')
+    else:
+        c_form = CreateCharacterForm()
     return render(request, 'manager/create_char.html')
 
 
 def create_account(request):
-    # a_form = CreateAccountForm()
-    return render(request, 'manager/create_char.html')
+    if request.method == "POST":
+        c_form = CreateAccountForm(request.POST)
+        if c_form.is_valid():
+            return redirect('home')
+    else:
+        c_form = CreateAccountForm()
+    return render(request, 'manager/create_acc.html')
+
+
+def update_date(request, name):
+    """Updates character's last_visited datefield."""
+    if request.method == "POST":
+        char = Character.objects.get(name=name)
+        char.last_visited = datetime.datetime.now()
+        char.save()
+        return redirect('home')
+
+
+def delete_char(request):
+    if request.method == "POST":
+        pass
+    return redirect('home')
