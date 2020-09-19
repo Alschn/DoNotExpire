9d9db4e809519204085c8cae6845927c71267e05
Alschn <adam.lisichin@gmail.com>
2020-09-19 23:20:54 +0200

    Update UI
    When you hover on the character div, it gets highlighted with a yellow border and the label above changes. Need to fix issue with using this feature for more than one account

---
+++ DoNotExpire/manager/static/manager/index.js
@@ -0,0 +1,12 @@
+function changeText(text)
+{
+    var display = document.getElementById('char-name-label');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+  function changeback(text)
+{
+    var display = document.getElementById('char-name-label');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
