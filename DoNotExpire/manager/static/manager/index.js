221337ac37f1973c6e0c62b584b4276e539d3e72
Alschn <adam.lisichin@gmail.com>
2020-09-22 18:07:45 +0200

    Add clickable Characters
    Added Char highlighting on click (Jquery). User can now have 1 char highlighted per account independently.

--- DoNotExpire/manager/static/manager/index.js
+++ DoNotExpire/manager/static/manager/index.js
@@ -4,3 +4,32 @@ function changeText(text, id)
     display.innerHTML = "";
     display.innerHTML = text;
 }
+
+// function enableDelete(char_id) {
+//     var delete_btn = document.getElementById('delete-btn-'+char_id)
+//     delete_btn.setAttribute("action", "")
+//     class_str = "{% url 'delete-char' " + char_id + " %}"
+//     delete_btn.setAttribute("action", class_str)
+// }
+
+
+$(document).ready(function (){
+    // get all char containers with a class
+    var charboxes = $('.clickable');
+    charboxes.click(function() {
+        // add border initially to the div where we clicked
+        $(this).addClass('border-yellow')
+        // get current account name
+        let acc_id = $(this).attr("id");
+        // get new querry only with 'local' chars
+        query = "#" + acc_id + ".clickable"
+        var chars = $(query)
+        // clickable div within current scope
+        chars.click(function() {
+            chars.removeClass('border-yellow')
+            $(this).addClass('border-yellow')
+        })
+    })
+
+})
+
