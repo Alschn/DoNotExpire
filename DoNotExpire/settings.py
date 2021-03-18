110c37cd8856cf0bbc99a1078e8cc12296a61d70
Alschn <adam.lisichin@gmail.com>
2021-03-18 14:19:52 +0100

    Update settings.py
    Added email settings to be configured/added later.

--- DoNotExpire/settings.py
+++ DoNotExpire/settings.py
@@ -128,3 +128,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+# configure later...
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''    # os.environ['DONOTEXPIRE_EMAIL']
+EMAIL_HOST_PASSWORD = ''    # os.environ['DONOTEXPIRE_EMAIL_PASS']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
