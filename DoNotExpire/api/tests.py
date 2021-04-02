4b5e82a7c60f04028be118a790e32a8b50081d9d
Alschn <adam.lisichin@gmail.com>
2021-04-02 17:25:19 +0200

    Add api tests, fix permissions
    Added api views unit tests, fixed issues with custom permissions, added more error handling.

--- DoNotExpire/api/tests.py
+++ DoNotExpire/api/tests.py
@@ -1,3 +1,94 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory, APIClient
 
-# Create your tests here.
+from DoNotExpire.profiles.models import Profile
+from DoNotExpire.manager.models import Account, Character, Equipment
+
+
+class TestAPIViews(TestCase):
+    def setUp(self) -> None:
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            'testuser', email='testuser@test.com', password='testing')
+        self.user.save()
+        self.account = Account.objects.create(
+            name="TestAcc", profile=self.user.profile, realm="Europe")
+        self.account.save()
+        self.character = Character.objects.create(
+            name="TestChar", level=90, char_class="Amazon", acc=self.account
+        )
+        self.character.save()
+
+    def _require_login(self):
+        self.client.login(username='testuser', password='testing')
+
+    def test_is_not_authenticated(self):
+        response = self.client.get(f'/api/chars/{self.character.name}')
+        self.assertEqual(response.status_code, 401)
+
+    def test_is_authenticated(self):
+        self._require_login()
+        response = self.client.get(f'/api/chars/{self.character.name}')
+        self.assertNotEqual(response.status_code, 403)
+
+    def test_is_char_owner_permission(self):
+        user = User.objects.create_user(
+            'sus', email='sus@test.com', password='testing123')
+        user.save()
+        self.client.login(username="sus", password="testing123")
+        response = self.client.get(f'/api/chars/{self.character.name}')
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_equipment_char_not_exists(self):
+        self._require_login()
+        test_charname = 'charthatdoesnotexist'
+        response = self.client.get(f'/api/chars/{test_charname}')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.data, {'Error': f"Character {test_charname} not found!"})
+
+    def test_get_equipment_which_does_not_exist(self):
+        char_wout_eq = Character.objects.create(
+            name="TestChar2", level=91, char_class="Paladin", acc=self.account
+        )
+        Equipment.objects.get(char=char_wout_eq).delete()
+
+        self._require_login()
+        response = self.client.get(f'/api/chars/{char_wout_eq.name}')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.data, {
+                'Error': f"{char_wout_eq.name}'s equipment not found!"}
+        )
+
+    def test_get_equipment(self):
+        self._require_login()
+        response = self.client.get(f'/api/chars/{self.character.name}')
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+        fields = [field.name for field in Equipment._meta.get_fields()]
+        self.assertEqual(
+            first=sorted(list(data.keys())),
+            second=sorted(fields)
+        )
+
+    def test_update_equipment_but_char_does_not_exist(self):
+        self._require_login()
+        test_charname = 'charthatdoesnotexist'
+        response = self.client.post(f'/api/equipments/{test_charname}')
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_equipment_but_it_does_not_exist(self):
+        self._require_login()
+        char_wout_eq = Character.objects.create(
+            name="TestChar3", level=92, char_class="Sorceress", acc=self.account
+        )
+        Equipment.objects.get(char=char_wout_eq).delete()
+        response = self.client.post(f'/api/equipments/{char_wout_eq.name}')
+        self.assertEqual(response.status_code, 404)
+
+    # def test_update_equipment(self):
+    #     self._require_login()
+    #     response = self.client.post(f'/api/equipments/{self.character.name}')
--- DoNotExpire/api/views.py
+++ DoNotExpire/api/views.py
@@ -1,6 +1,7 @@
 from DoNotExpire.manager.models import Character, Equipment
 from DoNotExpire.manager.serializers import EquipmentSerializer
 from rest_framework import status, permissions
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,23 +9,28 @@ from rest_framework.views import APIView
 
 class IsCharOwnerPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        try:
+        if "charname" in view.kwargs:
             charname = view.kwargs['charname']
-            char = Character.objects.get(name=charname)
-            if char in request.user.profile.get_all_characters():
+            char = Character.objects.filter(name=charname)
+            if not char.exists():
+                # permission is granted
+                # errors are handled inside the api view
                 return True
-            return False
-        except Exception:
-            return False
+            if not char.first() in request.user.profile.get_all_characters():
+                return False
+        return True
 
 
 class GetEquipment(APIView):
-    permission_classes = [IsAuthenticated | IsCharOwnerPermission]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated, IsCharOwnerPermission]
     serializer_class = EquipmentSerializer
 
     def get(self, request, charname):
-        char = Character.objects.get(name=charname)
-        eq = Equipment.objects.filter(char=char)
+        char = Character.objects.filter(name=charname)
+        if not char.exists():
+            return Response({'Error': f"Character {charname} not found!"}, status=status.HTTP_404_NOT_FOUND)
+        eq = Equipment.objects.filter(char=char.first())
         if not eq.exists():
             return Response({'Error': f"{charname}'s equipment not found!"}, status=status.HTTP_404_NOT_FOUND)
 
@@ -33,25 +39,29 @@ class GetEquipment(APIView):
 
 
 class UpdateEquipment(APIView):
-    permission_classes = [IsAuthenticated | IsCharOwnerPermission]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated, IsCharOwnerPermission]
     serializer_class = EquipmentSerializer
 
     def post(self, request, charname):
         char = Character.objects.filter(name=charname)
         if not char.exists():
-            return Response({'Error': 'Received incorrect charname!'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'Error': f'Character {charname} not found!'}, status=status.HTTP_404_NOT_FOUND)
         char = char.first()
+        eq = Equipment.objects.filter(char=char)
+        if not eq.exists():
+            return Response({'Error': f"{charname}'s equipment' not found!"}, status=status.HTTP_404_NOT_FOUND)
         # Link received data with a character model
         request.data._mutable = True
         request.data['char'] = char.pk
         request.data._mutable = False
 
         serializer = self.serializer_class(
-            instance=char.equipment, data=request.data)
+            instance=eq.first(), data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(char=char)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({'Error': 'Invalid serializer data!'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateCharacter(APIView):
