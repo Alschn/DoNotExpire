3de7c1a572b8aa464fd05506691f5ab59f9dce52
Alschn <67019937+Alschn@users.noreply.github.com>
2024-01-11 22:46:34 +0100

    Fix issue with selecting char on account with dot in name, update data attrs

--- manager/static/manager/js/index.js
+++ manager/static/manager/js/index.js
@@ -8,41 +8,49 @@ const changeText = (text, id) => {
 const maxCharCount = 18;
 
 // Get all the Create New Character buttons
-let create_btn = $(".btn-char-create");
-create_btn.each(function () {
+let createBtn = $(".btn-char-create");
+createBtn.each(function () {
+  const btnElement = $(this);
+
   // If there are 18 or more characters, disable the button
-  if (Number($(this).attr("data-char-count")) >= maxCharCount) {
-    $(this).attr("disabled", true);
-    $(this).addClass("btn-char-disabled");
+  if (Number(btnElement.attr("data-characters-count")) >= maxCharCount) {
+    btnElement.attr("disabled", true);
+    btnElement.addClass("btn-char-disabled");
   }
 });
 
 $(document).ready(function () {
-  // get all char containers with a class
-  let charboxes = $(".clickable");
-  charboxes.click(function () {
+  // get all char containers with a class, no mather what account
+  const allCharBoxes = $(".col-char.clickable");
+
+  allCharBoxes.click(function () {
+    const element = $(this);
+
     // add border initially to the div where we clicked
-    $(this).addClass("border-yellow");
-    // get current account name
-    let acc_id = $(this).attr("id");
+    element.addClass("border-yellow");
+
+    // get current account and character name
+    const accountName = element.attr("data-account-name");
+    const charName = element.attr("data-character-name");
 
     // change Delete button hidden input value field
-    let delete_form = $("#" + acc_id + "[name=char_id]");
-    let char_name = $(this).attr("data-char");
-    $(delete_form).val(char_name);
+    const hiddenInput = $(`input[name='char_id'][data-account-name='${accountName}']`);
+    $(hiddenInput).val(charName);
 
     // Toggle delete button
-    let delete_btn = delete_form.next(); // takes next element in DOM which is button in this case
-    $(delete_btn).removeClass("btn-char-disabled");
-    $(delete_btn).attr("disabled", false);
+    const deleteBtn = $(`button[data-account-name='${accountName}']`);
+    $(deleteBtn).removeClass("btn-char-disabled");
+    $(deleteBtn).attr("disabled", false);
 
-    // get new querry only with 'local' chars
-    let query = "#" + acc_id + ".clickable";
-    let chars = $(query);
-    // clickable div within current scope
-    chars.click(function () {
-      chars.removeClass("border-yellow");
-      $(this).addClass("border-yellow");
-    });
+    // get new query only with 'local' chars
+    const localCharBoxes = $(`.col-char.clickable[data-account-name='${accountName}']`);
+
+    localCharBoxes.removeClass("border-yellow");
+    localCharBoxes.attr("data-selected", false);
+    localCharBoxes.attr("aria-selected", false);
+
+    element.addClass("border-yellow");
+    element.attr("data-selected", true);
+    element.attr("aria-selected", true);
   });
 });
